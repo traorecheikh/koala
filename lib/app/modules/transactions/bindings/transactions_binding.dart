cc0eb3a1f678b2087df8c005ae99c3b86c94391e
traorecheikh <atidiane741@gmail.com>
2025-10-10 13:08:28 +0000

    feat: add enhanced  features

---
+++ lib/app/modules/transactions/bindings/transactions_binding.dart
@@ -0,0 +1,12 @@
+
+import 'package:get/get.dart';
+import 'package:koaa/app/modules/transactions/controllers/transactions_controller.dart';
+
+class TransactionsBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<TransactionsController>(
+      () => TransactionsController(),
+    );
+  }
+}
